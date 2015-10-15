7191f245eaae021a3df849af5575db0724afa2c2
Vitali Dettling <vitali.dettling@gmail.com>
2015-10-15 10:38:33 +0200

    Implemenation of the REST communication.

--- js/praxisboerse.js
+++ js/praxisboerse.js
@@ -13,6 +13,45 @@ var mainApp = angular.module('MainApp', []);//, 'ui.bootstrap']); <- TODO Later!
  */
 mainApp.controller('MainController', [ '$scope', function($scope) {
 
+   //TODO Delete
    $scope.GetStarted = "GetStarted";
 
+
+
 }]);
+
+mainApp.config(function($httpProvider) {
+   // Enable Cross-Domain-Communication
+   $httpProvider.defaults.useXDomain = true;
+   // Enable identification
+   $httpProvider.defaults.withCredentials = true;
+});
+
+/**
+ * Kapselung des Zugriffs in Form eines Dienstes.
+ * Noch einfacher klappt der Zugriff auf eine REST-Schnittstelle
+ * in vielen Faellen mit dem Modul ngResource.
+ */
+mainApp.factory('PraxisboerseService', [ '$http', function($http) {
+   var server = {};
+   var $base64;
+
+   return {
+
+      getAuthofication: function(user, pass) {
+         $http.defaults.headers.common.Authorization = "Basic" + $base64.encode(user + ":" + pass);
+         $http.post(url, 1234);
+      }
+
+   }
+
+
+
+
+
+}]);
+
+
+
+
+
