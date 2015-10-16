8a849f7151f1cc8958d6fd78ccdc4b7c336c9d13
Vitali Dettling <vitali.dettling@gmail.com>
2015-10-16 17:46:57 +0200

    Implemenation of the login information.

--- js/main.js
+++ js/main.js
@@ -1,22 +1,35 @@
 /**
  * Created by Vitali Dettling on 15.10.2015.
  */
-'use strict';
+'use strict'; //<= Was bedeudet das???                  ng-Config = Derictive???
 
-var mainApp = angular.module('mainApp', ['Praxisboerse']);//,'ui.bootstrap'
+var mainApp = angular.module('mainApp', ['base64']);//'Praxisboerse','ui.bootstrap'
 
 /**
  * Controller for the main page.
- */
-mainApp.controller('MainController', [ '$scope', function($scope) {
+*/
+mainApp.controller('MainController',['$base64', '$scope', '$http', function($base64, $scope, $http) {
 
     //TODO Debug information delete after the program is working.
     $scope.DependenciesWrong = "Dependencies are Working.";
     //TODO End Debug information. Delete
 
-    $scope.info = "Yes";
-    $scope.refresh = function() {
-        $scope.Buttom = "Pushed";
-        //$scope.$broadcast('incrementDate');???
-    };
+    var $base64;
+    var url = "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST";
+
+    $http.defaults.headers.common.Authorization = "Basic" + $base64.encode("User" + ":" + "Pass");
+    $http.post(url, 1234);
+
+    //TODO Here implement the API
+    $scope.API = $http.toString();
+
 }]);
+
+//Is the method called automatically???
+mainApp.config(function($httpProvider) {
+
+    // Enable Cross-Domain-Communication
+    $httpProvider.defaults.useXDomain = true;
+    // Enable identification
+    $httpProvider.defaults.withCredentials = true;
+});
--- praxisboerse/praxisboerse.js
+++ praxisboerse/praxisboerse.js
@@ -3,43 +3,6 @@
  */
 
 
-var praxisboerse = angular.module('Praxisboerse', []);
-
-
-praxisboerse.config(function($httpProvider) {
-   // Enable Cross-Domain-Communication
-   $httpProvider.defaults.useXDomain = true;
-   // Enable identification
-   $httpProvider.defaults.withCredentials = true;
-});
-
-/**
- * Kapselung des Zugriffs in Form eines Dienstes.
- * Noch einfacher klappt der Zugriff auf eine REST-Schnittstelle
- * in vielen Faellen mit dem Modul ngResource.
- */
-praxisboerse.factory('PraxisboerseService', [ '$http', function($http) {
-   var server = {};
-   var $base64;
-   var url = "https://www.iwi.hs-karlsruhe.de/Intranetaccess/REST";
-
-
-   return {
-
-      getAuthofication: function(user, pass) {
-         $http.defaults.headers.common.Authorization = "Basic" + $base64.encode(user + ":" + pass);
-         $http.post(url, 1234);
-      }
-
-   }
-
-
-
-
-
-}]);
-
-
 
 
 
